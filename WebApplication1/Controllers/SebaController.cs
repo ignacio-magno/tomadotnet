782267e712b22a4d37426bd6dc24aede31860ca8
Ignacio Larama <ignacio.larama@bailacthor.com>
2024-08-22 17:58:38 -0400

    Initial commit

---
+++ WebApplication1/Controllers/SebaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SebaController : ControllerBase
+{
+    [HttpGet("Ping")]
+    public string Ping()
+    {
+        return "Pong";
+    }
+}
